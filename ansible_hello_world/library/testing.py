986961f30adf646314a8e48c2e08d469e9a80073
andrewtian <adtian2@illinois.edu>
2020-05-21 11:03:02 -0500

    changed input method to receive files

--- ansible_hello_world/library/testing.py
+++ ansible_hello_world/library/testing.py
@@ -1,15 +1,16 @@
 #! /bin/env python
 from ansible.module_utils.basic import AnsibleModule
-import os, json
-import re, sys
+import os
+
 
 def firstProg(text):
     text1 = "Hello " + text
     return text1
 
+
 if __name__ == '__main__':
     fields = {
-    "yourName": {"required": True, "type": "str"}
+        "yourName": {"required": True, "type": "str"}
     }
     module = AnsibleModule(argument_spec=fields)
     yourName = os.path.expanduser(module.params['yourName'])
